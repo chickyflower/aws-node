6c6731a49390483af32116685c8ad78440d33339
Norma Gomez <chickyflower@gmail.com>
2022-02-18 20:49:30 +1300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const port = 3000;
 const server = http.createServer((req, res) => {
 	res.statusCode = 200;
-	res.setHeader("Content-Type", 'text/plain');
+	res.setHeader('Content-Type', 'text/plain');
 	res.end('Hello World');
 });
 
