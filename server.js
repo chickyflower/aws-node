1d7123c7cc1623467c104d4ccf4424346d7fba6b
Norma Gomez <chickyflower@gmail.com>
2022-02-18 21:07:11 +1300

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const http = require('http');
+
 const port = 3000;
+
 const server = http.createServer((req, res) => {
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/plain');
-	res.end('Hello World');
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
 });
 
 server.listen(port, () => {
-	console.log('Server running on port: ${port}');
+  console.log(`Server running on port: ${port}`);
 });
